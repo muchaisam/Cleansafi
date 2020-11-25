e7a63a48d335b19f9162f9f39fc1e790e74e9788
muchaisam <samson.muchai@strathmore.edu>
2020-11-25 10:39:48 +0300

    splashscreen

---
+++ app/src/main/java/com/example/cleansafi/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.cleansafi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
+public class SplashActivity extends Activity {
+     private TextView appName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        appName=findViewById(R.id.app_name);
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        },4000);
+    }
+}
