82166c60668aca55b58217266c4a9bd923fef980
muchaisam <samson.muchai@strathmore.edu>
2020-12-16 18:13:52 +0300

    payment activity

---
+++ app/src/main/java/com/example/cleansafi/PaymentActivity.java
@@ -0,0 +1,167 @@
+package com.example.cleansafi;
+
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.afollestad.materialdialogs.GravityEnum;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.MaterialDialog.ButtonCallback;
+
+import com.example.cleansafi.mpesa.Mpesa;
+import com.example.cleansafi.mpesa.interfaces.AuthListener;
+import com.example.cleansafi.mpesa.interfaces.MpesaListener;
+import com.example.cleansafi.mpesa.models.STKPush;
+import com.example.cleansafi.mpesa.utils.Pair;
+
+public class PaymentActivity extends AppCompatActivity implements AuthListener, MpesaListener {
+    //TODO: Replace these values from
+    public static final String BUSINESS_SHORT_CODE = "174379";
+    public static final String PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+    public static final String CONSUMER_KEY = "uPBOE7JMzzjfA5YAEusCuSlYT5ZCFSY5";
+    public static final String CONSUMER_SECRET = "sBPU94FxQ7lTux9J";
+    public static final String CALLBACK_URL = "http://mpesa-requestbin.herokuapp.com/x1ekuax1";
+
+
+    public static final String  NOTIFICATION = "PushNotification";
+    public static final String SHARED_PREFERENCES = "com.example.cleansafi.mpesa.utils.preferences";
+
+    Button pay;
+    ProgressDialog dialog;
+    EditText phone;
+    EditText amount;
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        pay = (Button)findViewById(R.id.pay);
+        phone = (EditText)findViewById(R.id.phone);
+        amount = (EditText)findViewById(R.id.amount);
+        Mpesa.with(this, CONSUMER_KEY, CONSUMER_SECRET);
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Processing");
+        dialog.setIndeterminate(true);
+
+        pay.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String p = phone.getText().toString();
+                int a = Integer.valueOf(amount.getText().toString());
+                if (p.isEmpty()){
+                    phone.setError("Enter phone.");
+                    return;
+                }
+                pay(p, a);
+            }
+        });
+
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(NOTIFICATION)) {
+                    String title = intent.getStringExtra("title");
+                    String message = intent.getStringExtra("message");
+                    int code = intent.getIntExtra("code", 0);
+                    showDialog(title, message, code);
+
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onAuthError(Pair<Integer, String> result) {
+        Log.e("Error", result.message);
+    }
+
+    @Override
+    public void onAuthSuccess() {
+
+        //TODO make payment
+        pay.setEnabled(true);
+    }
+    private void pay(String phone, int amount){
+        dialog.show();
+        STKPush.Builder builder = new STKPush.Builder(BUSINESS_SHORT_CODE, PASSKEY, amount,BUSINESS_SHORT_CODE, phone);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        String token = sharedPreferences.getString("InstanceID", "");
+
+        builder.setFirebaseRegID(token);
+        STKPush push = builder.build();
+
+
+
+        Mpesa.getInstance().pay(this, push);
+
+    }
+    private void showDialog(String title, String message,int code){
+        MaterialDialog dialog = new MaterialDialog.Builder(this)
+                .title(title)
+                .titleGravity(GravityEnum.CENTER)
+                .customView(R.layout.success_dialog, true)
+                .positiveText("OK")
+                .cancelable(false)
+                .widgetColorRes(R.color.colorPrimary)
+                .callback(new ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        super.onPositive(dialog);
+                        dialog.dismiss();
+                        finish();
+                    }
+                })
+                .build();
+        View view=dialog.getCustomView();
+        TextView messageText = (TextView)view.findViewById(R.id.message);
+        ImageView imageView = (ImageView)view.findViewById(R.id.success);
+        if (code != 0){
+            imageView.setVisibility(View.GONE);
+        }
+        messageText.setText(message);
+        dialog.show();
+    }
+
+    @Override
+    public void onMpesaError(Pair<Integer, String> result) {
+        dialog.hide();
+        Toast.makeText(this, result.message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMpesaSuccess(String MerchantRequestID, String CheckoutRequestID, String CustomerMessage) {
+        dialog.hide();
+        Toast.makeText(this, CustomerMessage, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(NOTIFICATION));
+
+    }
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+}
