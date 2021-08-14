ae129cb70f9cd01c0bf545ba4ba8c95c8eea8f2b
muchaisam <samson.muchai@strathmore.edu>
2021-08-14 11:00:38 +0300

    splash

--- app/src/main/java/com/example/cleansafi/SplashActivity.java
+++ app/src/main/java/com/example/cleansafi/SplashActivity.java
@@ -1,12 +1,11 @@
 package com.example.cleansafi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
+
 public class SplashActivity extends Activity {
      private TextView appName;
     @Override
