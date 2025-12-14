e1f7ceade9d7236d48577dd7da7b274b37cb25c2
muchaisam <samson.muchai@strathmore.edu>
2025-12-14 17:08:37 +0300

    feat: Clean up import statements and remove unnecessary blank lines in Converters.kt for improved readability

--- app/src/main/kotlin/com/cleansafi/data/local/Converters.kt
+++ app/src/main/kotlin/com/cleansafi/data/local/Converters.kt
@@ -8,19 +8,19 @@ import com.cleansafi.domain.model.ServiceType
 class Converters {
     @TypeConverter
     fun fromServiceType(value: ServiceType): String = value.name
-    
+
     @TypeConverter
     fun toServiceType(value: String): ServiceType = ServiceType.valueOf(value)
-    
+
     @TypeConverter
     fun fromLaundryItemType(value: LaundryItemType): String = value.name
-    
+
     @TypeConverter
     fun toLaundryItemType(value: String): LaundryItemType = LaundryItemType.valueOf(value)
-    
+
     @TypeConverter
     fun fromOrderStatus(value: OrderStatus): String = value.name
-    
+
     @TypeConverter
     fun toOrderStatus(value: String): OrderStatus = OrderStatus.valueOf(value)
 }
