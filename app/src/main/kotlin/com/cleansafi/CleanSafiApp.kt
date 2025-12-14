5a48db6c17ad0ae983924e07c747fe076d4055fa
muchaisam <samson.muchai@strathmore.edu>
2025-12-14 17:29:26 +0300

    feat: Refactor CleanSafiApp for improved readability and code consistency

--- app/src/main/kotlin/com/cleansafi/CleanSafiApp.kt
+++ app/src/main/kotlin/com/cleansafi/CleanSafiApp.kt
@@ -3,4 +3,5 @@ package com.cleansafi
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp class CleanSafiApp : Application()
+@HiltAndroidApp
+class CleanSafiApp : Application()
