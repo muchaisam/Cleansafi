1026860985097a64c32cbdd2992b9659072880dc
muchaisam <samson.muchai@strathmore.edu>
2025-12-14 17:28:29 +0300

    feat: Clean up whitespace in PricingCalculator.kt for improved code consistency

--- app/src/main/kotlin/com/cleansafi/presentation/order/placeorder/PricingCalculator.kt
+++ app/src/main/kotlin/com/cleansafi/presentation/order/placeorder/PricingCalculator.kt
@@ -4,7 +4,7 @@ import com.cleansafi.domain.model.LaundryItemType
 import com.cleansafi.domain.model.ServiceType
 
 object PricingCalculator {
-    
+
     fun calculateItemPrice(itemType: LaundryItemType, serviceType: ServiceType): Int {
         return when (serviceType) {
             ServiceType.IRON_ONLY -> when (itemType) {
@@ -13,12 +13,14 @@ object PricingCalculator {
                 LaundryItemType.BEDSHEETS -> 50
                 LaundryItemType.OTHERS -> 60
             }
+
             ServiceType.WASH_ONLY -> when (itemType) {
                 LaundryItemType.TOPS -> 40
                 LaundryItemType.TROUSERS -> 50
                 LaundryItemType.BEDSHEETS -> 60
                 LaundryItemType.OTHERS -> 70
             }
+
             ServiceType.WASH_AND_IRON -> when (itemType) {
                 LaundryItemType.TOPS -> 50
                 LaundryItemType.TROUSERS -> 60
@@ -27,7 +29,7 @@ object PricingCalculator {
             }
         }
     }
-    
+
     fun calculateTotalPrice(
         itemQuantities: Map<LaundryItemType, Int>,
         serviceType: ServiceType
