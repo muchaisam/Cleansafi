03fc8cb9ff1822606263b74a19b24c4170131ec4
muchaisam <samson.muchai@strathmore.edu>
2025-12-14 09:28:20 +0300

    Add ValidationResult sealed class for validation handling

---
+++ app/src/main/kotlin/com/cleansafi/core/validation/ValidationResult.kt
@@ -0,0 +1,12 @@
+package com.cleansafi.core.validation
+
+sealed class ValidationResult {
+    data object Valid : ValidationResult()
+    data class Invalid(val message: String) : ValidationResult()
+    
+    val isValid: Boolean
+        get() = this is Valid
+        
+    val errorMessage: String?
+        get() = (this as? Invalid)?.message
+}
